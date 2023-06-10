fa7196096d3803ee23403e6d50830473e0b60254
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-06-10 18:31:56 -0300

    Criação da pagina que é resposavel por mostrar todas as tasks que foram concluidas

--- src/pages/Completed/index.tsx
+++ src/pages/Completed/index.tsx
@@ -1,7 +1,21 @@
+import { Task } from '../../components/Task';
+import { useContext } from 'react';
+import { TaskContext } from '../../contexts/TaskContexts';
+import { Container, Content } from './styles';
+
 export function Completed() {
+  const { tasks } = useContext(TaskContext);
+
+  const tasksCompleted = tasks.filter((task) => task.completed === true);
   return (
     <>
-      <h1>Completed</h1>
+      <Container>
+        <Content>
+          {tasksCompleted.map((task) => (
+            <Task key={task.id} data={task} />
+          ))}
+        </Content>
+      </Container>
     </>
   );
 }
---
+++ src/pages/Completed/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+
+export const Container = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+`
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 20rem;
+    overflow-y: auto;
+    overflow-x: none;
+`
