687604a07a3d62a0f8d375d606d18c1fb3f44dba
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-07-05 01:04:01 -0300

    Alteração na estilização da scroll view da aplicação

--- src/global.ts
+++ src/global.ts
@@ -23,4 +23,34 @@ export const GlobalStyle = createGlobalStyle`
         font-weight: 400;
         font-size: 1rem;
     }
+
+::-webkit-scrollbar {
+  width: 8px;
+  border-radius: 8px;
+}
+
+::-webkit-scrollbar-track {
+  background-color: #1A1A1A;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #262626;
+  border-radius: 8px;
+}
+
+scrollbar {
+  width: 8px;
+  border-radius: 8px;
+  margin-top: 1rem;
+}
+
+scrollbar[orient="vertical"] {
+  background-color: #1A1A1A;
+}
+
+scrollbar-thumb {
+  background-color: #262626;
+  border-radius: 8px;
+}
+
 `
