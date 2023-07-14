fa122eedb9d82cba9d197e96971792f5e0d6d5f1
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-07-14 01:23:53 -0300

    Criação de um contexto para o tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/theme/default';
+// import { ThemeProvider } from 'styled-components';
+// import { defaultTheme } from './styles/theme/default';
 import { GlobalStyle } from './global';
 import { TaskContextProvider } from './contexts/TaskContexts';
-
 import { ToastContainer } from 'react-toastify';
+
+import { ThemeContextProvider } from './contexts/ThemeContext';
+
 import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeContextProvider>
       <BrowserRouter>
         <TaskContextProvider>
           <Router />
@@ -18,6 +20,6 @@ export function App() {
       </BrowserRouter>
       <GlobalStyle />
       <ToastContainer autoClose={2000} />
-    </ThemeProvider>
+    </ThemeContextProvider>
   );
 }
---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect, createContext } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme, lightTheme } from '../styles/theme/default';
+
+export const ThemeContext = createContext({} as ThemeContextProps);
+
+interface ThemeContextProps {
+  theme: string;
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
+  toggleTheme: () => void;
+}
+
+interface ChildrenProps {
+  children: React.ReactNode;
+}
+
+export function ThemeContextProvider({ children }: ChildrenProps) {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    const currentTheme = localStorage.getItem('@theme');
+    if (currentTheme) {
+      setTheme(currentTheme);
+    }
+  }, []);
+
+  function toggleTheme() {
+    if (theme === '') {
+      localStorage.setItem('@theme', 'light');
+      setTheme('light');
+    } else {
+      localStorage.setItem('@theme', 'dark');
+      setTheme('');
+    }
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : defaultTheme}>
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+}
