51fa0e0159ce80ea1ac4aaf45b27422f486cf310
Silas Silva <54956902+SilasSilvaa@users.noreply.github.com>
2023-08-07 23:41:42 -0300

    refactor - Alteração na tipagem

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,17 @@
 import { SVGProps } from 'react';
 import { ButtonComponent } from './styled';
+import { Task } from '../../contexts/TaskContexts';
 
 interface ButtonProps {
   content: string;
   svg: SVGProps<SVGSVGElement>;
   colorButton: 'default' | 'delete';
-  onAction?: (task: any) => void;
+  onAction?: (task?: Task) => void;
 }
 
 export function Button({ colorButton, content, svg, onAction }: ButtonProps) {
   return (
-    <ButtonComponent colorButton={colorButton} onClick={onAction}>
+    <ButtonComponent colorButton={colorButton} onClick={() => onAction?.()}>
       <>
         {content}
         {svg}
